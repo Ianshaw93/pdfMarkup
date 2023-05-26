b9a4946cf6d19a1dbb87f503d1a2664db3ecca49
Ian Shaw <ianshaw@firedynamicsgroup.com>
2023-05-26 07:51:37 +0100

    added pdf to canvas background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
 import './App.css'
 import Canvas from './Components/Canvas'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+// import { pdfjs } from "react-pdf";
+// import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist/es5/build/pdf';
+// import PDFJSWorker from 'pdfjs-dist/es5/build/pdf.worker.entry';
+
+const pdfjs = await import('pdfjs-dist/build/pdf');
+const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+// GlobalWorkerOptions.workerSrc = PDFJSWorker;
+
 
 function App() {
   const [ previewImage, setPreviewImage ] = useState();
+  const pdfCanvasRef = useRef()
 
+  function handleFileChange(event) {
+    const file = event.target.files[0]
+    if (file && pdfCanvasRef.current) {
+      setPreviewImage(true)
+      const loadingTask = pdfjs.getDocument(URL.createObjectURL(file))
+      loadingTask.promise.then((pdf) => {
+
+        const pageNumber = 1
+        pdf.getPage(pageNumber).then((page) => {
+          console.log("setting up pdf")
+          const canvas = pdfCanvasRef.current
+          const context = canvas.getContext("2d")
+  
+          const scale = 1.5
+          const viewport = page.getViewport({ scale: scale })
+  
+            // Prepare canvas using PDF page dimensions
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+  
+            // Render PDF page into canvas context
+            var renderContext = {
+              canvasContext: context,
+              viewport: viewport,
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(() => {
+              console.log("Page rendered");
+            });
+            
+        })
+      })
+    }  
+  }
   return (
     <>
     <div>
@@ -13,17 +58,8 @@ function App() {
           id="image"
           name="image"
           type="file"
-          accept="image/*"
-          onChange={({target}) => {
-            if (target.files[0]) {
-              const file = target.files[0];
-              const reader = new FileReader();
-              reader.onloadend = () => {
-                setPreviewImage(reader.result);
-              };
-              reader.readAsDataURL(file);
-            }
-          }}
+          accept=".pdf"
+          onChange={handleFileChange}
         />
       </label>
 
@@ -32,16 +68,20 @@ function App() {
       { previewImage ? (<>
         <div className='pointer-events-none'> 
         {/* unclear why height of img is not changed */}
-        <img 
+        {/* {previewImage && <img 
         src={previewImage}
         className='inset-0 absolute z-0 pointer-events-none user-drag-none'
         height={window.innerHeight}
         width={window.innerWidth}
-        />
+        />} */}
         </div>
         <Canvas tool={"polyline"}/>
       </>
       ) : <div> Please upload image </div>}
+      <canvas 
+      ref={pdfCanvasRef}
+      className='z-100'
+      />
     </div>
     </>
   )
